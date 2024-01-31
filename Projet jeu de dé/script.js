bfa159b5fb151b4589c6f4127733b5b885ea2db4
elmy1979 <139561524+elmy1979@users.noreply.github.com>
2024-01-31 12:20:24 +0100

    Add files via upload

---
+++ Projet jeu de dé/script.js
@@ -0,0 +1,38 @@
+/*Variable DOM*/
+const player_0=document.querySelector(".wrapper player-player_0");
+const player_1=document.querySelector(".wrapper player-player_1");
+const score_0=document.getElementById("score_0");
+const score_1=document.getElementById("score_1");
+const diceE1=document.querySelector(".dice");
+let scoreGlobalPlayer_0=document.querySelector('.global global-player_0')
+let scoreGlobalPlayer_1=document.querySelector('.global global-player_1')
+const scoreCurrentPlayer_0=document.getElementById(".round round-current_0");
+const scoreCurrentPlayer_1=document.getElementById(".round round-current_1");
+//Variables
+    
+//Les boutons
+const btnRoll=document.querySelector("btn_roll");
+const btnHold=document.querySelector("btn_hold");
+const btnNew=document.querySelector("btn_new");
+score_0.textContent=43;
+score_1.textContent=72;
+current_0.textContent=11;
+current_1.textContent=0;
+let scores=[0,0]
+let activePlayer=0;
+let currentScore=0;
+let playGame=true;
+
+
+
+
+
+
+
+
+
+    
+
+
+
+   
